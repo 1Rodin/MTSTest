0f1bf4bcafafd35e4685686aa5c0653c7b4c3ad2
Andrey Rodin <andrey-rodin1@yandex.ru>
2022-05-25 17:06:41 +0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,19 @@
+SELECT 
+	p.ClientName,
+	MONTH(p."Date") AS "Month",
+	(
+		SELECT SUM(po.Amount)
+		FROM [Post] po
+		WHERE 
+			po.ClientName = p.ClientName 
+			AND YEAR(po."Date") = YEAR(p."Date") 
+			AND MONTH(po."Date") <= MONTH(p."Date") 
+	) AS "SumAmount"
+FROM [Post] p
+WHERE YEAR(p."Date") = 2017
+GROUP BY p.ClientName, YEAR(p."Date"), MONTH(p."Date")
+ORDER BY ClientName desc, MONTH(p."Date")
+
+
+
+[POST] - название таблицы, писать create и insert сюда не стал
