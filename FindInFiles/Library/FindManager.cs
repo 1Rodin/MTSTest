def80172f5c66da0e853f72e05678a080128e642
1Rodin <andrey-rodin1@yandex.ru>
2022-05-25 16:48:37 +0300

    Add project files.

---
+++ FindInFiles/Library/FindManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FindInFiles.Library
+{
+
+    public class Result
+    {
+        public string FileName { get; set; }
+        public string Operation { get; set; }
+        public object Value { get; set; }
+        public override string ToString() => $"{Path.GetFileName(FileName)}-{Operation}-{Value}";
+    }
+   public class FindManager
+    {
+        public object DoCSS(string text)
+        {
+            var open = text.Count(a => a.Equals('{'));
+            var close = text.Count(a => a.Equals('}'));
+            return open == close;
+        }
+
+        public Result ParsCSS(string path)
+        {
+            var text = File.ReadAllText(path);
+            var result = DoCSS(text);
+            return new Result
+            {
+                FileName = path,
+                Operation = "CSS",
+                Value = result
+            }; 
+        }
+
+        public object DoHTML(string text)
+        {
+             Regex div = new Regex(@"<\s*div[^>]*>(.*?)<\s*/\s*div>");
+             var matches = div.Matches(text).Count;
+            return matches;
+
+        }
+
+        public Result ParseHTML(string path)
+        {
+           
+            var text = File.ReadAllText(path);
+
+            var result = DoHTML(text);
+
+            return new Result
+            {
+                FileName = path,
+                Operation = "HTML",
+                Value = result
+            };
+        }
+
+
+        public Result ParseDef(string path)
+        {
+            var text = File.ReadAllText(path);
+            
+            var result = DoDef(text);
+
+            return new Result
+            {
+                FileName = path,
+                Operation = "TXT",
+                Value = result
+            };
+        }
+
+        public object DoDef(string text)
+        {
+             char[] _marks = { '.', ',', '!', '?', ';', ':', '"', '(', ')', '\'' };
+            var cnt = text.Count(a => _marks.Contains(a));
+            return cnt;
+        }
+
+
+
+
+    }
+}
---
+++ FindInFiles/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindInFiles.Library;
+
+namespace FindInFiles
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Выберите тип файла введя номер");
+            Console.WriteLine("1. CSS\n2. HTML\n3. TXT");
+            var typefile = Console.ReadLine();
+
+            switch (typefile)
+            {
+                case "1":
+                    CSS();
+                    break;
+                case "2":
+                    HTML();
+                    break;
+                case "3":
+                    Def();
+                    break;
+                default:
+                    Console.WriteLine("Нет такого файла");
+                    break;
+
+            }
+
+
+
+            void CSS()
+            {
+                Console.WriteLine("Введите путь к файлу полностью");
+                FindManager m = new FindManager();
+                var filepath = Console.ReadLine().Trim('"');
+                var Extension = ".css";
+                if (filepath.Contains(Extension))
+                {
+                    try
+                    {
+                        var dir = Path.GetDirectoryName(filepath);
+                        var res = m.ParsCSS(filepath);
+                        Console.WriteLine(res);
+                        var save = res.ToString();
+                        var repPath = Path.Combine(dir, save+".txt");
+                        using( FileStream fs = File.Create(repPath)) {
+                            byte[] re = new UTF8Encoding(true).GetBytes(res.Value.ToString());
+                            fs.Write(re, 0, re.Length);
+                        }                        
+                       
+                        Console.ReadLine();
+                    }
+                    catch (Exception Ex)
+                    {
+                        Console.WriteLine("ОШИБКА:" + Ex.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Файл не соответствует");
+                    Console.ReadLine();
+                }
+
+            }
+
+            void HTML()
+            {
+                Console.WriteLine("Введите путь к файлу полностью");
+                FindManager m = new FindManager();
+                var filepath = Console.ReadLine().Trim('"');
+                var Extension = ".html";
+                if (filepath.Contains(Extension))
+                {
+                    try
+                    {
+                        var dir = Path.GetDirectoryName(filepath);
+                        var res = m.ParseHTML(filepath);
+                        Console.WriteLine(res);
+                        var save = res.ToString();
+                        var repPath = Path.Combine(dir, save + ".txt");
+                        using (FileStream fs = File.Create(repPath))
+                        {
+                            byte[] re = new UTF8Encoding(true).GetBytes(res.Value.ToString());
+                            fs.Write(re, 0, re.Length);
+                        }
+                        Console.ReadLine();
+                    }
+                    catch (Exception Ex)
+                    {
+                        Console.WriteLine("ОШИБКА:" + Ex.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Файл не соответствует");
+                    Console.ReadLine();
+                }
+            }
+
+            void Def()
+            {
+                Console.WriteLine("Введите путь к файлу полностью");
+                FindManager m = new FindManager();
+                var filepath = Console.ReadLine().Trim('"');
+                var Extension = ".txt";
+                if (filepath.Contains(Extension))
+                {
+                    try
+                    {
+                        var dir = Path.GetDirectoryName(filepath);
+                        var res = m.ParseDef(filepath);
+                        Console.WriteLine(res);
+                        var save = res.ToString();
+                        var repPath = Path.Combine(dir, save + ".txt");
+                        using (FileStream fs = File.Create(repPath))
+                        {
+                            byte[] re = new UTF8Encoding(true).GetBytes(res.Value.ToString());
+                            fs.Write(re, 0, re.Length);
+                        }
+                        Console.ReadLine();
+                    }
+                    catch (Exception Ex)
+                    {
+                        Console.WriteLine("ОШИБКА:" + Ex.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Файл не соответствует");
+                    Console.ReadLine();
+                }
+            }
+
+
+
+
+
+
+        }
+
+
+    }
+}
